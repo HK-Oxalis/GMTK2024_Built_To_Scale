70cf0195209b97134f6ea857d9fcb4cf167381c6
probabl <hsfk2017@gmail.com>
2024-08-17 17:57:32 +0100

    Fixed cursor infinite loop

--- Grid_Cursor.cs
+++ Grid_Cursor.cs
@@ -8,12 +8,13 @@ public partial class Grid_Cursor : Node2D
     [Export] Grid grid;
     [Export] float ui_Cooldown = 0.1f;
     private Timer timer;
-    public Vector2 cell {get => cell; set => Set_Cell(value); }
+
+    private Vector2 _cell;
+    public Vector2 cell {get => _cell; set => Set_Cell(value); }
     
     
     public override void _Ready()
     {
-        GD.Print("cursor ready");
         timer = (Timer)this.FindChild("Timer");
         timer.WaitTime = ui_Cooldown;
         
@@ -23,14 +24,18 @@ public partial class Grid_Cursor : Node2D
 
     public override void _Input(InputEvent @event)
     {
+        GD.Print("Running input check");
         //Check the cooldown on repeated events 
         //(if the event was there last time and the timer is not finished)
+        
         if(@event.IsEcho() && !timer.IsStopped()){
             return;
         }
         if(@event is InputEventMouseMotion motion_Event){
-    
+            GD.Print("Mouse Movement Detected");
+            GD.Print(grid.Calculate_Grid_Position(motion_Event.Position));
             cell = grid.Calculate_Grid_Position(motion_Event.Position);
+            GD.Print("Mouse movement resolved");
         }
         if(@event.IsActionPressed("Accept") || @event.IsActionPressed("ui_accept")){
             EmitSignal(SignalName.CellInteracted, cell);
@@ -43,16 +48,18 @@ public partial class Grid_Cursor : Node2D
 
     
     public void Set_Cell(Vector2 new_Cell){
+        GD.Print("Custom setter starts");
         new_Cell = grid.Grid_Clamp(new_Cell);
 
         if(new_Cell.IsEqualApprox(this.cell)){
             return;
         }
 
-        cell = new_Cell;
-        this.Position = grid.Calculate_World_Position(cell);
+        _cell = new_Cell;
 
+        this.Position = grid.Calculate_World_Position(new_Cell);
         timer.Start();
+        
     }
 
 
